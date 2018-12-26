294849900982592260fe566b8f3e36042cf3cf0d
Nathan Miles <ntmls@live.com>
2018-12-25 21:18:00 -0600

    Inverse kinimatics.

--- V2.js
+++ V2.js
@@ -13,17 +13,39 @@ function V2(x, y) {
 
 var VADD = function(a, b) {
     return new V2(a.x + b.x, a.y + b.y);
-}
+};
 
 var VFLIP = function(a) {
     return new V2(-a.x, -a.y);
-}
+};
 
 var VLEN = function(a) {
     return Math.sqrt(a.x * a.x + a.y * a.y);
-}
+};
 
 var VNORM = function(a) {
     var len = VLEN(a);
     return new V2(a.x / len, a.y / len);
-}
+};
+
+var VMULT = function(vect, scale) {
+    return new V2(vect.x * scale, vect.y * scale);
+};
+
+var VDOT = function(a, b) {
+    return a.x * b.x + a.y * b.y;
+};
+
+var D2R = function(degrees) {
+    return degrees * (Math.PI / 180);
+};
+
+var R2D = function(radians) {
+    return radians * (180 / Math.PI);
+};
+
+var VROT = function(vect, radians) {
+    var ca = Math.cos(radians);
+    var sa = Math.sin(radians);
+    return new V2(ca * vect.x - sa * vect.y, sa * vect.x + ca * vect.y);
+};
